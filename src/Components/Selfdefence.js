8906f958d710af38e0a6e94814104a1141509b86
amisha64 <amishakirti6410@gmail.com>
2020-12-11 19:06:47 +0530

    Added section

---
+++ src/Components/Selfdefence.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import M from "materialize-css"
+
+
+function Selfdefence() {
+    return (
+        <div>
+            <h1>BASIC SELF DEFENCE MOVES</h1>
+        </div>
+    )
+}
+
+export default Selfdefence
